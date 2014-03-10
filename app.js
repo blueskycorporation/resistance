4a691f04c0dab7aa2f9f521fd80caed86594b697
blueskycorporation <blueskycorporation@gmail.com>
2014-03-09 20:19:01 -0500

    implement sessions in order to store data between page refresh and disconnections

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -15,10 +14,20 @@ var runninggame = require('./routes/runninggame');
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
+var MemoryStore = express.session.MemoryStore,
+	sessionStore = new MemoryStore();
 
+//var parseCookie = require('connect').utils.parseCookie;
 
+/**
+ * Load external classes
+ */
 
 var Game = require('./Game.js').Game;
+
+/**
+ * Create server instance
+ */
 
 var app = express();
 
@@ -26,19 +35,67 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+var server = app.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+var io = socketio.listen(server);
+
+/**
+ *  Configure app for sessions
+ */
+
+app.configure(function () {
+    app.use(express.cookieParser());
+    app.use(express.session({store: sessionStore
+        , secret: 'secret'
+        , key: 'express.sid'}));
+    
+});
+
+var cookie = require('cookie');
+var connect = require('connect');
+var Session = connect.middleware.session.Session;
+
+// override handshake to store session
+io.set('authorization', function (data, accept) {
+	if (data.headers.cookie) {
+		data.cookie = connect.utils.parseSignedCookies(cookie.parse(decodeURIComponent(data.headers.cookie)),'secret');
+		data.sessionID = data.cookie['express.sid'];
+		// save the session store to the data object 
+		// (as required by the Session constructor)
+		data.sessionStore = sessionStore;
+		sessionStore.get(data.sessionID, function (err, session) {
+			if (err || !session) {
+				accept('Error', false);
+				
+			} else {
+				// create a session object, passing data as request and our
+				// just acquired session data
+				data.session = new Session(data, session);
+				accept(null, true);
+			}
+		});
+	} else {
+		return accept('No cookie transmitted.', false);
+	}
+});
+
+
+/**
+ * Configure app
+ */
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
+/**
+ * Configure routes
+ */
 app.get('/', routes.index);
 app.get('/create_game', create_game.main);
 app.get('/users', user.list);
@@ -47,11 +104,12 @@ app.get('/debug', debug.main);
 app.get('/login', login.main);
 app.get('/player', player.main);
 app.get('/runninggame', runninggame.main);
+app.use(app.router);
 
-var server = app.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-var io = socketio.listen(server);
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 /* Associative arrays.
  * players[username] = socketId;
@@ -62,7 +120,25 @@ var io = socketio.listen(server);
 var players = {};
 var socketsOfPlayers = {};
 
+// When a client connects to the server
 io.sockets.on('connection', function(socket) {
+	
+	var hs = socket.handshake;
+	console.log('A socket with sessionID ' + hs.sessionID + ' connected!');
+	
+	// setup an inteval that will keep our session fresh
+	var intervalID = setInterval(function () {
+		// reload the session (just in case something changed,
+		// we don't want to override anything, but the age)
+		// reloading will also ensure we keep an up to date copy
+		// of the session with our connection.
+		hs.session.reload( function () { 
+			// "touch" it (resetting maxAge and lastAccess)
+			// and save it back again.
+			hs.session.touch().save();
+		});
+	}, 60 * 1000);
+	
 	
 	/*
 	 * Handle client to server communication here
